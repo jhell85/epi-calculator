ef8a2ec76a0dc4323c486167cc1f6e0a09cd7452
josh <jhell85@gmail.com>
2020-03-23 12:00:28 -0700

    added js to buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,39 @@
 $(document).ready(function() {
-  
+  let currentNumber = ""
+  $("#1").click(function() {
+    currentNumber += "1"
+  })
+  $("#2").click(function() {
+    currentNumber += "2"
+  })
+  $("#3").click(function() {
+    currentNumber += "3"
+  })
+  $("#4").click(function() {
+    currentNumber += "4"
+  })
+  $("#5").click(function() {
+    currentNumber += "5"
+  })
+  $("#6").click(function() {
+    currentNumber += "6"
+  })
+  $("#7").click(function() {
+    currentNumber += "7"
+  })
+  $("#8").click(function() {
+    currentNumber += "8"
+  })
+  $("#9").click(function() {
+    currentNumber += "9"
+  })
+  $("#0").click(function() {
+    currentNumber += "0"
+  })
+  $("#equals").click(function() {
+    console.log(currentNumber);
+    
+  })
+
 });
   
